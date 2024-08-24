e181d1eaebd9a5beb4e1edd18449c42c51cb19d4
bhagwan <ikkarbhagwan@gmail.com>
2024-08-24 16:20:00 +0530

    vishnu is here

--- App.js
+++ App.js
@@ -1,20 +1,11 @@
-import React from "react";
-import RactDOM from "react-dom/client"
+ import React from "react";
+import ReactDOM from "react-dom/client"
 
-const parent=React.createElement("div",{id:"parent"},[
-    React.createElement("div",{id:"child"},[
-        React.createElement("h1",{},"im h1 tag"),
-        React.createElement("h2",{},"im h2 tag"),
-    ]),    
-    React.createElement("div",{id:"child2"},[
-        React.createElement("h1",{},"im h1 tag"),
-        React.createElement("h2",{},"im h2 tag"),
-    ])
+const heading =React.createElement("h1",{id:"heading"},"Vishnu is here and there");
 
-]);
+console.log(heading);
 
-console.log(parent);
+const root=ReactDOM.createRoot(document.getElementById("root"));
 
-const root=RactDOM.createRoot(document.getElementById("root"));
+root.render;
 
-root.render(parent);
